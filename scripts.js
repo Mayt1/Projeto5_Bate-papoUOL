41e8844a4dd99f8011dd5b69e9d2aee93ec9a541
VictorUlian <yctor.ulian@gmail.com>
2022-02-18 05:10:02 -0300

    Este commite adiciona o menu da aplicaçao

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 let username;
+let logMessage = [];
 registerUsername();
 
 function registerUsername() {
     username = { name: prompt('Qual o seu nome?') };
-    let promiseUsername = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', username);
-    promiseUsername.then(sucess);
-    promiseUsername.catch(error);
+    let promisseUsername = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', username);
+    promisseUsername.then(sucess);
+    promisseUsername.catch(error);
 }
 
 function sucess() {
@@ -21,10 +22,104 @@ function error(erro) {
 }
 
 function maintainConection() {
-    let promiseConection = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', username);
-    promiseConection.then(sucess);
-    promiseConection.catch(error);
+    let promisseConection = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', username);
+    promisseConection.then(sucess);
+    promisseConection.catch(error);
     console.log('Tudo certo coneccao');
 }
 
 setInterval(maintainConection, 5000);
+
+function sendMessage() {
+    let message = messageArea.value;
+    if (message === "") {
+        return;
+    }
+    let newObjetcMessage = { from: username.name, to: "Todos", text: message, type: 'message' };
+    let promisseObjetc = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", newObjetcMessage);
+    messageArea.value = "";
+    promisseObjetc.then(sucess);
+    promisseObjetc.catch(error);
+
+}
+
+function pressSendMessage() {
+    let messageArea = document.querySelector("#messageArea"); //pelo id
+    messageArea.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            sendMessage();
+        }
+    })
+}
+
+pressSendMessage()
+
+setInterval(searchMessage, 3000);
+
+function searchMessage() {
+    const promisseSearch = axios.get(`https://mock-api.driven.com.br/api/v4/uol/messages`);
+    promisseSearch.then(response => {
+      renderingMessage(response.data);
+      focusLastMessage();
+    });
+    promisseSearch.catch( erro => {
+      alert("Erro na hora de receber menssages!");
+    })
+  }
+
+//==============================================
+function renderingMessage(messages) {
+    const ul = document.querySelector("main ul");
+    ul.innerHTML = "";
+  
+    messages.forEach( message => {
+      const type = message.type;
+      const from = message.from;
+      const to = message.to;
+      const time = message.time;
+      const text = message.text;
+  
+      let messageHTML = null;
+      if(type === "status") {
+        messageHTML = `
+        <li class="message status data-identifier="message"">
+          <span class="time">(${time})</span>
+          <span class="person"><b>${from}</b></span>
+          <span class="text">${text}</span>
+        </li>
+        `
+      } else {
+        if(type === "message") {
+          messageHTML = `
+          <li class="message public data-identifier="message"">
+          <span class="time">(${time})</span>
+            <span class="person"><b>${from}</b> para <b>${to}</b>: </span>
+            <span class="text">${text}</span>
+          </li>
+          `;
+        } else {
+          
+          if(from === username || to === username){
+            messageHTML = `
+            <li class="message private data-identifier="message"">
+            <span class="time">(${time})</span>
+              <span class="person"><b>${from}</b> reservadamente para <b>${to}</b>: </span>
+              <span class="text">${text}</span>
+            </li>
+            `;
+          }
+        }
+      }
+  
+      if(messageHTML !== null) {
+        ul.innerHTML += messageHTML;
+      }
+    })
+  }
+  
+  function focusLastMessage() {
+    const ul = document.querySelector("main ul");
+    const ultimamessage = ul.lastElementChild;
+    ultimamessage.scrollIntoView();
+  }
+
